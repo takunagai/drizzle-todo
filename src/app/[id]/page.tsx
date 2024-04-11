5fb437b78590995b09581b7a5c66996f34ec9d40
takna <tak_na@icloud.com>
2024-04-11 21:16:59 +0900

    詳細ページに一覧に戻るリンクを追加
    src/app/[id]/page.tsxファイルに"一覧に戻る"のリンクを追加しました。このリンクによって、ユーザーはTODOの詳細ページからTODOの一覧ページに簡単に戻ることができます。また、Propsの定義をtypeに変更しました。

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,24 +1,27 @@
+import Link from 'next/link'
 import DetailTodo from "@/components/DetailTodo"
 import DeleteTodo from "@/components/DeleteTodo"
 import getTodo from "../actions/getTodo"
 
-interface Props {
-  id: string
+type Props = {
+    id: string;
 }
-const Page = async ({ params }: { params: Props }) => {
-  const todo = await getTodo(params)
 
-  return (
-    <div className="p-12">
-      <div className="flex flex-row justify-between mb-12">
-        <h1 className="text-[24px] font-bold">TODO詳細</h1>
-        <DeleteTodo />
-      </div>
-      <div className="max-w-[800px] w-full mx-auto">
-        <DetailTodo todo={todo} />
-      </div>
-    </div>
-  )
+const Page = async ({params}: { params: Props }) => {
+    const todo = await getTodo(params)
+
+    return (
+        <div className="p-12">
+            <div className="flex flex-row justify-between mb-12">
+                <p><Link href="../" className="text-sm underline">» 一覧に戻る</Link></p>
+                <h1 className="text-[24px] font-bold">TODO詳細</h1>
+                <DeleteTodo/>
+            </div>
+            <div className="max-w-[800px] w-full mx-auto">
+                <DetailTodo todo={todo}/>
+            </div>
+        </div>
+    )
 }
 
 export default Page
