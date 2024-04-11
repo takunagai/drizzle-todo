0f7d4b73b6e3c0e0bdc084a8907710230f81b341
takna <tak_na@icloud.com>
2024-04-11 19:51:21 +0900

    TODO詳細ページのベースを追加
    新たにTodoの詳細を表示するページを作成しました。そのため、特定のIDを持つTodoの情報を取得する動作も実装されました。現在は、Todoの詳細と削除機能はコメントアウトされています。

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,24 @@
+// import { DetailTodo } from "@/features/todo/DetailTodo"
+import getTodo from "../actions/getTodo"
+// import { DeleteTodo } from "@/features/todo/DeleteTodo"
+
+interface Props {
+  id: string
+}
+const Page = async ({ params }: { params: Props }) => {
+  const todo = await getTodo(params)
+
+  return (
+    <div className="p-12">
+      <div className="flex flex-row justify-between mb-12">
+        <h1 className="text-[24px] font-bold">TODO詳細</h1>
+        {/*<DeleteTodo />*/}
+      </div>
+      <div className="max-w-[800px] w-full mx-auto">
+        {/*<DetailTodo todo={todo} />*/}
+      </div>
+    </div>
+  )
+}
+
+export default Page
